69488b5368193d7ec28834c07ef2097a2646d6c3
Allan Schneck <143731856+AllanSchneck@users.noreply.github.com>
2025-10-20 20:45:56 -0300

    corrigindo erro de importação

--- src/app/api/books/route.js
+++ src/app/api/books/route.js
@@ -2,7 +2,6 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
   const books = await prisma.book.findMany({ orderBy: { createdAt: "desc" } });
