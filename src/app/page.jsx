07b1544836899e14d141524adf48eff9dcd3bca2
Allan Schneck <143731856+AllanSchneck@users.noreply.github.com>
2025-10-20 22:36:49 -0300

    rename url

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ import ContainerPage from "./components/ContainerPage"
 export default async function Home() {
   const cookieHeader = cookies().toString();
 
-  const res = await fetch("http://localhost:3000/api/auth/me", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}http://localhost:3000/api/auth/me`, {
     headers: { Cookie: cookieHeader },
     cache: "no-store",
   });
